abc02278d25d8509d45d473e55d1c69ce4c60801
Yazooyy <46268191+Yazooyy@users.noreply.github.com>
2019-01-13 09:39:56 -0500

    Create stokeley.js

---
+++ src/commands/stokeley.js
@@ -0,0 +1,5 @@
+const Discord = require('discord.js');
+
+exports.run = (client, message, args) => {
+    message.channel.send('iTunes • https://itunes.apple.com/us/album/stokeley/1444949614\nSpotify • https://open.spotify.com/album/0z0z4DcXhHiobX5ZKAw8Qn\nYouTube • https://www.youtube.com/watch?v=PbsjNX27eJ0\nPandora • https://www.pandora.com/artist/ski-mask-the-slump-god/stokeley-explicit/ALtgg26Jhbrbr9K')
+};
